ed2a86df69d6ceb7ccb56403d047283673f3c103
kbinani <kbinani.bt@gmail.com>
2022-04-26 05:31:34 +0900

    Remove demo window

--- src/render.hpp
+++ src/render.hpp
@@ -3,17 +3,11 @@
 namespace nbte {
 
 struct State {
-    bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 };
 
 static void Render(State& s) {
-    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-    if (s.show_demo_window) {
-        ImGui::ShowDemoWindow(&s.show_demo_window);
-    }
-
     // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
     {
         static float f = 0.0f;
@@ -22,7 +16,6 @@ static void Render(State& s) {
         ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
         ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-        ImGui::Checkbox("Demo Window", &s.show_demo_window);      // Edit bools storing our window open/close state
         ImGui::Checkbox("Another Window", &s.show_another_window);
 
         ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
