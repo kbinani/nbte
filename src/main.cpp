f19a380e182b6f9ed8484b69131a2a4e8f170615
kbinani <kbinani.bt@gmail.com>
2022-04-26 06:57:54 +0900

    Fix compile errors on macOS

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
 #endif
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
-#include "defer.hpp"
 #include "render.hpp"
 
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
