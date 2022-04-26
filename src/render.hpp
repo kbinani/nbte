19d28dca259b57a1b0179cacf5b069449af710d9
kbinani <kbinani.bt@gmail.com>
2022-04-27 07:31:51 +0900

    Render IntTag values

--- src/render.hpp
+++ src/render.hpp
@@ -58,9 +58,75 @@ static void RenderErrorPopup(State &s) {
   }
 }
 
+static void VisitCompoundTag(State const &s, mcfile::nbt::CompoundTag const &tag, unsigned int depth) {
+  using namespace std;
+  using namespace ImGui;
+  using namespace mcfile::nbt;
+
+  constexpr float kIndent = 8.0f;
+  constexpr float kArrowWidth = 21.0f;
+
+  auto const &style = GetStyle();
+  float currentIndent = depth * kIndent;
+
+  for (auto &it : tag) {
+    string const &name = it.first;
+    switch (it.second->type()) {
+    case Tag::Type::Int: {
+      if (auto v = dynamic_pointer_cast<IntTag>(it.second); v) {
+        auto textSize = CalcTextSize(name.c_str());
+        Indent(kArrowWidth);
+        Text(name.c_str());
+        SameLine();
+        PushItemWidth(s.fDisplaySize.x - textSize.x - kArrowWidth - 30 * depth - 32);
+        InputInt("", &v->fValue);
+        Unindent(kArrowWidth);
+        PopItemWidth();
+      }
+      break;
+    }
+    case Tag::Type::Compound: {
+      if (auto v = dynamic_pointer_cast<CompoundTag>(it.second); v) {
+        SetNextItemOpen(true, ImGuiCond_Once);
+        if (TreeNode(name.c_str())) {
+          Indent(kIndent);
+          VisitCompoundTag(s, *v, depth + 1);
+          TreePop();
+          Unindent(kIndent);
+        }
+      }
+      break;
+    }
+    }
+  }
+}
+
+static void RenderCompoundTag(State &s) {
+  using namespace std;
+  using namespace ImGui;
+  using namespace mcfile::nbt;
+
+  if (s.fOpened.index() != 0) {
+    return;
+  }
+  shared_ptr<CompoundTag> const &tag = get<0>(s.fOpened);
+  if (!tag) {
+    return;
+  }
+
+  ImGuiWindowFlags flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove;
+  Begin("compound_tag", nullptr, flags);
+  float height = GetFrameHeight();
+  SetWindowPos(ImVec2(0, height));
+  SetWindowSize(ImVec2(s.fDisplaySize.x, s.fDisplaySize.y - height));
+  VisitCompoundTag(s, *tag, 0);
+  End();
+}
+
 static void Render(State &s) {
   RenderMainMenu(s);
   RenderErrorPopup(s);
+  RenderCompoundTag(s);
   ImGui::Render();
 }
 
