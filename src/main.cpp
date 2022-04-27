ed7f913a93c74021bce84c1b0aedd157088a082d
kbinani <kbinani.bt@gmail.com>
2022-04-27 19:23:10 +0900

    Change window title

--- src/main.cpp
+++ src/main.cpp
@@ -65,7 +65,7 @@ int main(int, char **) {
 #endif
 
   // Create window with graphics context
-  GLFWwindow *window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(1280, 720, "nbte", NULL, NULL);
   if (window == NULL)
     return 1;
   glfwMakeContextCurrent(window);
