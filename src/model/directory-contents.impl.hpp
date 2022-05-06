860657af5dacc41ddcc88a66ae544ab940c03bab
kbinani <kbinani.bt@gmail.com>
2022-05-06 18:36:38 +0900

    Sort directory contents

--- src/model/directory-contents.impl.hpp
+++ src/model/directory-contents.impl.hpp
@@ -3,18 +3,28 @@
 namespace nbte {
 
 DirectoryContents::DirectoryContents(Path const &dir, std::shared_ptr<Node> parent) : fDir(dir) {
+  using namespace std;
   namespace fs = std::filesystem;
-  std::error_code ec;
+  error_code ec;
   auto iterator = fs::directory_iterator(dir, ec);
   if (ec) {
     return;
   }
+  map<String, Path> directories;
+  map<String, Path> files;
   for (auto const &it : iterator) {
+    Path p = it.path();
     if (it.is_directory()) {
-      fValue.push_back(Node::DirectoryUnopened(it.path(), parent));
+      directories[p.filename().u8string()] = p;
     } else if (it.is_regular_file()) {
-      fValue.push_back(Node::FileUnopened(it.path(), parent));
+      files[p.filename().u8string()] = p;
     }
+  }
+  for (auto const &it : directories) {
+    fValue.push_back(Node::DirectoryUnopened(it.second, parent));
+  }
+  for (auto const &it : files) {
+    fValue.push_back(Node::FileUnopened(it.second, parent));
   }
 }
 
